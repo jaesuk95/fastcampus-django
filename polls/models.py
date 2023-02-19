3f6dae5a25c0a17e85ebd35438be9c3748fa0d8f
Jaesuk <jaesuk95@outlook.com>
2023-02-19 15:56:30 +0900

    views extension & Models(Entity) setting

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+# db 에는 Question, Choice 두 엔티티가 있다
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
--- polls/views.py
+++ polls/views.py
@@ -3,5 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+# index 요청
 def index(request):
+    # 중간에 비즈니스 로직
     return HttpResponse("Hello, world. You are at the polls index")
+
+
+def test(request):
+    return HttpResponse("Hello, world. You are at the test index")
